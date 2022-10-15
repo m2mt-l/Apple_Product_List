48ef50c5806e00add3c5941bc8959d80094e68cd
m2mt-l <ta28ma2@gmail.com>
2022-10-15 07:19:10 +0000

    add ipad item data

---
+++ src/data/ipadItemData.ts
@@ -0,0 +1,13 @@
+import { AppleItem } from "../model/AppleItem";
+
+export const macItemData: AppleItem[] = [
+    { imageUrl: "", name: "iPad Pro", description: "The ultimate iPad experience.", price: 799 },
+    { imageUrl: "", name: "iPad Air", description: "Powerful. Colorful. Wonderful.", price: 599 },
+    {
+        imageUrl: "",
+        name: "iPad",
+        description: "Delightfully capable. Surprisingly affordable.",
+        price: 329,
+    },
+    { imageUrl: "", name: "iPad mini”", description: "Mega power. Mini sized.", price: 499 },
+];
