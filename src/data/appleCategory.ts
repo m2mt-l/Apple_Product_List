aebb468a2943a3a650b4aff30792bbf416c2f29a
m2mt-l <ta28ma2@gmail.com>
2022-10-15 07:07:10 +0000

    add category and mac data

---
+++ src/data/appleCategory.ts
@@ -0,0 +1,13 @@
+interface Category {
+    mac: string;
+    ipad: string;
+    iphone: string;
+    watch: string;
+}
+
+export const appleCategory: Category = {
+    mac: "Mac",
+    ipad: "iPad",
+    iphone: "iPhone",
+    watch: "Watch",
+};
---
+++ src/data/macItemData.ts
@@ -0,0 +1,17 @@
+interface Item {
+    imageUrl: string;
+    name: string;
+    description: string;
+    price: number;
+}
+
+export const macItemData: Item[] = [
+    { imageUrl: "", name: "MacBook Air", description: "M1", price: 999 },
+    { imageUrl: "", name: "MacBook Air", description: "M2", price: 1199 },
+    { imageUrl: "", name: "MacBook Pro", description: "", price: 1299 },
+    { imageUrl: "", name: "MacBook Pro 14” and 16”", description: "", price: 1999 },
+    { imageUrl: "", name: "iMac 24", description: "", price: 1299 },
+    { imageUrl: "", name: "Mac mini", description: "", price: 699 },
+    { imageUrl: "", name: "Mac Studio", description: "", price: 1999 },
+    { imageUrl: "", name: "Mac Pro", description: "", price: 5999 },
+];
