05f0e76d2bb4a7ce7513fdbaf007aab4d8b5fc61
m2mt-l <ta28ma2@gmail.com>
2022-10-15 09:46:17 +0000

    feat: add category name

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -1,5 +1,11 @@
 import React from "react";
+import { AppleCategory } from "../model/AppleCategory";
 
-class Category extends React.Component {}
+class Category extends React.Component<AppleCategory> {
+    render(): React.ReactNode {
+        const name = this.props.name;
+        return <div className="container mx-auto overflow-x-auto text-2xl font-bold">{name}</div>;
+    }
+}
 
 export default Category;
