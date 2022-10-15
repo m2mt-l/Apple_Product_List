81c38974f78d1fe3b778944a942856d281b423ed
m2mt-l <ta28ma2@gmail.com>
2022-10-15 07:29:15 +0000

    add watch item data

---
+++ src/data/watchItemData.ts
@@ -0,0 +1,7 @@
+import { AppleItem } from "../model/AppleItem";
+
+export const watchItemData: AppleItem[] = [
+    { imageUrl: "", name: "Apple Watch Series 8", description: "", price: 399 },
+    { imageUrl: "", name: "Apple Watch SE", description: "", price: 249 },
+    { imageUrl: "", name: "Apple Watch Ultra", description: "", price: 799 },
+];
