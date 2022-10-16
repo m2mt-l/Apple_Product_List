b4b3082efd4ce99af732aaf9562d67e723d0a13c
m2mt-l <ta28ma2@gmail.com>
2022-10-16 02:12:00 +0000

    add Item and Category

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -1,10 +1,30 @@
 import React from "react";
+import Item from "./Item";
 import { AppleCategory } from "../model/AppleCategory";
+import { AppleItem } from "../model/AppleItem";
+import { macItemData } from "../data/macItemData";
 
-class Category extends React.Component<AppleCategory> {
+class Category extends React.Component<AppleCategory, AppleItem> {
     render(): React.ReactNode {
         const name = this.props.name;
-        return <div className="container mx-auto overflow-x-auto text-2xl font-bold">{name}</div>;
+        return (
+            <div className="text-2xl font-bold">
+                {name}
+                <div className="flex flex-start container mx-auto overflow-x-auto">
+                    {macItemData.map((item) => {
+                        return (
+                            <Item
+                                key={item.name}
+                                imageUrl={item.imageUrl}
+                                name={item.name}
+                                description={item.description}
+                                price={item.price}
+                            />
+                        );
+                    })}
+                </div>
+            </div>
+        );
     }
 }
 
--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,5 +1,38 @@
 import React from "react";
+import { AppleItem } from "../model/AppleItem";
+class Item extends React.Component<AppleItem> {
+    render(): React.ReactNode {
+        const imageUrl = this.props.imageUrl;
+        const name = this.props.name;
+        const description = this.props.description;
+        const price = this.props.price;
 
-class Item extends React.Component {}
+        return (
+            <div className="mt-6 py-8 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+                <div className="group relative">
+                    <div className="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-auto rounded-md bg-gray-200 group-hover:opacity-75 lg:w-80 lg:h-80">
+                        <img
+                            src={imageUrl}
+                            alt=""
+                            className="h-full w-full object-cover object-center"
+                        />
+                    </div>
+                    <div className="mt-4 flex justify-between">
+                        <div>
+                            <h3 className="text-sm text-gray-700">
+                                <a href="#">
+                                    <span aria-hidden="true" className="absolute inset-0"></span>
+                                    {name}
+                                </a>
+                            </h3>
+                            <p className="mt-1 text-sm text-gray-500">{description}</p>
+                        </div>
+                        <p className="text-sm font-medium text-gray-900">${price}</p>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
 
 export default Item;
